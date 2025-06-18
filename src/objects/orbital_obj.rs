9b8b4e8d1aa713f24da7bcf659c0ac817111fd66
Sarah Keghian <sarah.keghian@etu.univ-amu.fr>
2025-06-18 09:55:26 +0200

    feat: add From<MainBodyData> for OrbitalObjects

--- src/objects/orbital_obj.rs
+++ src/objects/orbital_obj.rs
@@ -1,20 +1,29 @@
 use bevy::prelude::*;
 use crate::objects::{ships, bodies};
+use crate::objects::bodies::main_bodies::MainBodyData;
+
+#[derive(PartialEq, Debug)]
 enum OrbitalObjID {
     Body(bodies::BodyID),
     Ship(ships::ShipID),
 }
 
-#[derive(Component)]
-pub(crate) struct OrbitingObjects(Vec<OrbitalObjID>);
+#[derive(Component, PartialEq, Debug)]
+pub(crate) struct OrbitingObjects(pub(crate) Vec<OrbitalObjID>);
 
-impl OrbitingObjects {
-    
-    pub(crate) fn to_orbiting_objects(orbiting_bodies: &Vec<bodies::BodyID>) -> Self {
+impl From<&MainBodyData> for OrbitingObjects {
+    fn from(value: &MainBodyData) -> Self {
+        let orbiting_bodies: Vec<_> = value
+            .orbiting_bodies
+            .iter()
+            .map(|b| Into::<bodies::BodyID>::into(b.clone()))            
+            .collect();
+
         let orbiting_obj = orbiting_bodies
-        .iter()
-        .map(|body_id| {OrbitalObjID::Body(body_id.clone())})
-        .collect::<Vec<_>>();
-    Self(orbiting_obj)
+            .iter()
+            .map(|body_id| {OrbitalObjID::Body(body_id.clone())})
+            .collect::<Vec<_>>();
+
+        Self(orbiting_obj)
     }
 }
